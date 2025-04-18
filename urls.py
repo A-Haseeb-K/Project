789224fad402c8cee19ca11a5db77cda655b1f48
Abdul Haseeb Khan <165879584+A-Haseeb-K@users.noreply.github.com>
2025-04-18 17:51:48 +0500

    Add files via upload
    commit #2

--- urls.py
+++ urls.py
@@ -1,24 +1,14 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path,include
+from app.api import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('' , views.Dashboard , name='dashboard'),
-    path('registration/',views.Registration , name='registration'),
-    path('login/' , views.Login , name='login'),
-    path('logout/' , views.Logout , name = 'logout'),
-    path('image/' , views.Images , name = 'images'),
-    path('video/' , views.Videos , name = 'videos'),
-    path('audio/' , views.Audios , name = 'audios'),
-    path('search/' , views.Searchimage , name = 'search'),
-    path('chat/' , views.Chat , name = 'chat'),
-    path('types/' , views.Types , name = 'types'),
-    path('song/' , views.Songs , name = 'songs'),
-    path('songtype/' , views.Songtype , name = 'songtype'),
-    path('latest/' , views.Latests , name = 'latest'),
-    path('hollywood/' , views.Hollywoods , name = 'hollywood'),
-    path('bollywood/' , views.Bollywoods , name = 'bollywood'),
-    path('south/' , views.Souths , name = 'south'),
-    path('engweb/' , views.Engwebs , name = 'engweb'),
-    path('hindiweb/' , views.Hindiwebs , name = 'hindiweb'),
+    path('admin/', admin.site.urls),
+    path('',include('app.urls')),
+    path ('api/' , include('app.api.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT) 
